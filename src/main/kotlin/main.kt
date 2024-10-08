f88cd017fe09163f3039f2eab7de7138e17f7f8e
Aydar <hispooy@gmail.com>
2024-10-08 23:22:51 +0500

    Start task

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,27 +1,36 @@
+import com.sun.org.apache.xml.internal.serializer.utils.Utils
+import com.sun.org.apache.xml.internal.serializer.utils.Utils.messages
+
 fun main() {
+    val messageService = MessageService()
+    val message = Message(0, 1, false, "text", false)
+    messageService.create(message)
+    println(message)
+    val count = messageService.getUnreadChatsCount()
+println (count)
 }
 
 data class Message(
     var id: Int = 0, // id сообщения
-    val idChat: Int = 0, // название собеседника
+    val idChat: Int, // название собеседника
     var isDeleted: Boolean = false, //  удаленность сообщения
     var text: String = "0",//  само сообщение
-    var readMessage: Boolean = false // прочитано сообщение?
+    var readMessage: Boolean = false, // прочитано сообщение?
+    val sent: Boolean = true // отправленное или полученное сообщение
 )
 
 data class Chat(
     var id: Int = 0, // id сообщения
     val title: String = "Name", // название собеседника
     var isDeleted: Boolean = false,//  удаленность чата
-    val message:Message = Message()
+    val message: Message,
 
-)
+    )
 
 
 interface Service<T> {
     fun create(item: T): T
     fun read(id: Int): T?
-
     fun delete(id: Int): Boolean
     fun show()
 
@@ -33,17 +42,17 @@ class MessageService(private val messages: MutableList<Message> = mutableListOf(
     override fun create(message: Message): Message {
         message.id = i++
         messages.add(message)
-        val chatService  = ChatService()
-        chatService.create(Chat(message = message ))
+        val chatService = ChatService()
+        chatService.create(Chat(message = message))
 
         return message
     }
 
-    override fun read(id: Int): Message? {
+    override fun read(id: Int): Message? { //+
         return messages.find { it.id == id }
     }
 
-    fun update(message: Message): Message? {
+    fun update(message: Message): Message? { //+
         val index = messages.indexOfFirst { it.id == message.id }
         if (index != -1) {
             messages[index] = message
@@ -77,6 +86,22 @@ class MessageService(private val messages: MutableList<Message> = mutableListOf(
             false
         }
     }
+
+    fun getUnreadChatsCount(): (List<Message>) -> Int {
+
+
+        val predicate = fun(message: Message) = message.readMessage == false
+        val unreadList = messages.filter(predicate)
+
+        val uniqueId: (List<Message>) -> Int = { idChat -> idChat.distinct().size }
+        return uniqueId
+
+    }
+//    public inline fun <Message> MutableList<Message>.filter (predicate : (Message) -> Boolean): List<Message> {
+//
+//        return filterTo(ArrayList<Message>(),predicate )
+//
+//    }
 }
 
 
@@ -93,10 +118,6 @@ class ChatService(private val chats: MutableList<Chat> = mutableListOf()) : Serv
         return chats.find { it.id == id }
     }
 
-   fun  getUnreadChatsCount () : Int? {
-       var count = 0
-       return count
-   }
 
     override fun delete(id: Int): Boolean {
         val chat = read(id)
@@ -113,11 +134,13 @@ class ChatService(private val chats: MutableList<Chat> = mutableListOf()) : Serv
             println("ID: ${chat.id}, Текст: ${chat.title}")
         }
     }
-    fun  getLastChatsCount () : Int? {
+
+    fun getLastChatsCount(): Int {
         var count = 0
         return count
     }
-     fun showId() {
+
+    fun showId() {
         for (chat in chats) {
             println("ID: ${chat.id}, Текст: ${chat.title}")
         }
